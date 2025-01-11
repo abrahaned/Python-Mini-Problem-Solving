acf82d540aa9c4828d7668d6d37990eef2083518
arbhaned <salaithomas@beedatamyanmar.com>
2025-01-11 14:04:35 +0630

    naming refactor

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -3,13 +3,13 @@ dice = [1,2,3,4,5,6]
 playing = True
 
 while playing:
-    is_keep = input("Roll the dice? (y/n):").lower()
-    if is_keep == "n":
+    choice = input("Roll the dice? (y/n):").lower()
+    if choice == "n":
         print('Thank You!')
         playing = False
-    elif is_keep == "y":
-        num1 = random.choice(dice)
-        num2 = random.choice(dice)
-        print(f'({num1},{num2})')
+    elif choice == "y":
+        dice1 = random.choice(dice)
+        dice2 = random.choice(dice)
+        print(f'({dice1},{dice2})')
     else:
         print("Invalid")
