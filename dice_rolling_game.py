e4410c4e0557f6f2b2ad4959cfbeea055e3bd462
arbhaned <salaithomas@beedatamyanmar.com>
2025-01-11 15:03:20 +0630

    version-2

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -1,15 +1,12 @@
 import random
-dice = range(1,7)
-playing = True
 
-while playing:
+while True:
     choice = input("Roll the dice? (y/n): ").lower().strip()
     if choice == "n":
         print('Thank You For Playing!')
-        playing = False
+        break
     elif choice == "y":
-        dice1 = random.choice(dice)
-        dice2 = random.choice(dice)
+        dice1,dice2 = random.randint(1,6),random.randint(1,6)
         print(f'({dice1},{dice2})')
     else:
         print("Invalid choice! Please enter 'y' or 'n'.")
