44f3bcea1a2e5ce9df98e18770e667f46bfc9c7a
SalaiThomas <salaithomas582@gmail.com>
2025-02-05 08:33:27 +0630

    refactor with check_win_loose

--- rock_paper_sissor.py
+++ rock_paper_sissor.py
@@ -8,91 +8,54 @@ import random
 item = ["r","p","s"]
 is_continue = ["y","n"]
 
-while True:
-    user_choice = input("Rock, Paper , or sissor ? (r/p/s) : ")
-    if user_choice in item:
-        computer = random.choice(item)
-        if user_choice == computer:
-            print(f"You Choose {user_choice}")
-            print(f"Computer Choose {computer}")
-            print("Drew")
-            keep_play = input("Continue? (y/n)")
-            if keep_play in is_continue:
-                if keep_play == "y":
-                    continue
-                else:
-                    break
-            else:
-                print("Invalid")
-        else:
-            if user_choice == "r":
-                if computer == "p":
+def check_win_loose(user,comp):
+    if user == comp:
+        print(f"You Choose {user_choice}")
+        print(f"Computer Choose {computer}")
+        print("Drew")
+    else:
+        if user_choice == "r":
+                if comp == "p":
                     print(f"You Choose {user_choice}")
                     print(f"Computer Choose {computer}")
                     print("You Loose!")
-                    keep_play = input("Continue? (y/n)")
-                    if keep_play in is_continue:
-                        if keep_play == "y":
-                            continue
-                        else:
-                            break
-                    else:
-                        print("Invalid")
                 else:
                     print(f"You Choose {user_choice}")
                     print(f"Computer Choose {computer}")
                     print("You Win!")
-            elif user_choice == "p":
-                    if computer == "r":
-                        print(f"You Choose {user_choice}")
-                        print(f"Computer Choose {computer}")
-                        print("You Win!")
-                        keep_play = input("Continue? (y/n)")
-                        if keep_play in is_continue:
-                            if keep_play == "y":
-                                continue
-                            else:
-                                break
-                        else:
-                            print("Invalid")
-                    else:
-                        print(f"You Choose {user_choice}")
-                        print(f"Computer Choose {computer}")
-                        print("You Loose!")
-                        keep_play = input("Continue? (y/n)")
-                        if keep_play in is_continue:
-                            if keep_play == "y":
-                                continue
-                            else:
-                                break
-                        else:
-                            print("Invalid")
-            elif user_choice == "s":
-                if computer == "p":
-                        print(f"You Choose {user_choice}")
-                        print(f"Computer Choose {computer}")
-                        print("You Win!")
-                        keep_play = input("Continue? (y/n)")
-                        if keep_play in is_continue:
-                            if keep_play == "y":
-                                continue
-                            else:
-                                break
-                        else:
-                            print("Invalid")
-                else:
+        elif user_choice == "p":
+                if computer == "s":
                     print(f"You Choose {user_choice}")
                     print(f"Computer Choose {computer}")
                     print("You Loose!")
-                    keep_play = input("Continue? (y/n)")
-                    if keep_play in is_continue:
-                        if keep_play == "y":
-                            continue
-                        else:
-                            break
-                    else:
-                        print("Invalid")       
-
+                else:  
+                    print(f"You Choose {user_choice}")
+                    print(f"Computer Choose {computer}")
+                    print("You Win!")
+        elif user_choice == "s":   
+            if computer == "r":
+                print(f"You Choose {user_choice}")
+                print(f"Computer Choose {computer}")
+                print("You Loose!")
+            else:
+                print(f"You Choose {user_choice}")
+                print(f"Computer Choose {computer}")
+                print("You Win!")
+        
+while True:
+    user_choice = input("Rock, Paper , or sissor ? (r/p/s) : ")
+    
+    if user_choice in item:
+        computer = random.choice(item)
+        
+        check_win_loose(user_choice,computer)
+        
+        keep_play = input("Continue ? (y/n) : ")
+        if keep_play in is_continue:
+            if keep_play == "y":
+                continue
+        else:
+            ("Invalid")
+            
     else:
         print("Invalid")
-        continue
