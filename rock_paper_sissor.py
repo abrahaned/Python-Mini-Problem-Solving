3ae33e8121072590d24ec2a65a82017501283c5e
SalaiThomas <salaithomas582@gmail.com>
2025-02-08 11:29:27 +0630

    overall refactor

--- rock_paper_sissor.py
+++ rock_paper_sissor.py
@@ -1,50 +1,26 @@
 import random
 item = ["r","p","s"]
-is_continue = ["y","n"]
 
 def check_win_loose(user,comp):
     if user == comp:
         print(f"You Choose {user_choice}")
         print(f"Computer Choose {computer}")
-        print("Drew")
+    if user == comp:
+        print("Draw")
+    elif (user == "r" and comp == "s") or (user == "p" and comp == "r") or (user == "s" and comp == "p"):
+        print("You Win!")
     else:
-        if user_choice == "r":
-                if comp == "p":
-                    print(f"You Choose {user_choice}")
-                    print(f"Computer Choose {computer}")
-                    print("You Loose!")
-                else:
-                    print(f"You Choose {user_choice}")
-                    print(f"Computer Choose {computer}")
-                    print("You Win!")
-        elif user_choice == "p":
-                if computer == "s":
-                    print(f"You Choose {user_choice}")
-                    print(f"Computer Choose {computer}")
-                    print("You Loose!")
-                else:  
-                    print(f"You Choose {user_choice}")
-                    print(f"Computer Choose {computer}")
-                    print("You Win!")
-        elif user_choice == "s":   
-            if computer == "r":
-                print(f"You Choose {user_choice}")
-                print(f"Computer Choose {computer}")
-                print("You Loose!")
-            else:
-                print(f"You Choose {user_choice}")
-                print(f"Computer Choose {computer}")
-                print("You Win!")
-
+        print("You Lose!")
+        
 def is_quit():
-    keep_play = input("Continue ? (y/n) : ").lower()
-    if keep_play in is_continue:
+    while True:
+        keep_play = input("Continue ? (y/n) : ").lower()
         if keep_play == "y":
             return False
-        else:
+        elif keep_play == "n":
             return True
-    else:
-        ("Invalid")  
+        else:
+            print("Invalid! Please Enter y or n")  
               
 while True:
     user_choice = input("Rock, Paper , or sissor ? (r/p/s) : ").lower()
